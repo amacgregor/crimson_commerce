9af64fa03eef80dfa47f932871cd396d7eb7a51f
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-11-22 13:54:40 -0500

    refactor: move category navigation retrieval to view
    Issue: https://github.com/amacgregor/crimson_commerce/issues/7

--- apps/crimson_commerce_web/lib/crimson_commerce_web/views/layout_view.ex
+++ apps/crimson_commerce_web/lib/crimson_commerce_web/views/layout_view.ex
@@ -1,3 +1,10 @@
 defmodule CrimsonCommerceWeb.LayoutView do
   use CrimsonCommerceWeb, :view
+
+  alias CrimsonCommerce.Catalog
+
+
+  def category_list do
+    Catalog.list_categories()
+  end
 end
